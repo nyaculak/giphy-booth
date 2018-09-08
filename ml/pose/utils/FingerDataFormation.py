546c7dd6f98bf7a8194152cc2c7d153c63555b5c
Nick Yaculak <nyaculak@gmail.com>
2018-09-08 14:05:23 -0400

    Removing ml code

--- ml/pose/utils/FingerDataFormation.py
+++
@@ -1,8 +0,0 @@
-class FingerDataFormation:
-    def __init__(self):
-        self.curl_position = []
-        self.finger_position = []
-        self.curl_position_confidence = []
-        self.finger_position_confidence = []
-        self.position_name = ''
-        self.position_id = -1
